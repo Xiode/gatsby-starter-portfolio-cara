ef6c8c47b163f7319d30de4195d4e1723ff5459a
Thomas H <Xiodetheone@gmail.com>
2022-07-18 23:20:33 -0600

    first try

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Stone Electric - Castle Rock, Littleton, and Denver Electrician`,
+    siteTitleAlt: `Stone Electric - Castle Rock Electrician`,
+    siteHeadline: `Stone Electric - Castle Rock Electrician`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `Need an Electrician?  (303) 123-4567  Service, repair, upgrade, remodel.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
